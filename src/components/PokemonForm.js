469ec3650b12416f31334dbcba6a3b98010cafdf
askoenig <askoenig94@gmail.com>
2019-07-09 12:21:06 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -8,10 +8,41 @@ class PokemonForm extends React.Component {
     this.state = {
       name: '',
       hp: '',
-      frontUrl: '',
-      backUrl: ''
+      sprites: {
+      front: '',
+      back: ''
     }
   }
+  }
+
+  handleChange = (event) => {
+     this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+//   handleSubmit = (event) => {
+
+//     event.preventDefault();
+//     fetch('http://localhost:3000/pokemon', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type':'application/json',
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify({
+//       "name": this.state.name,
+//       "hp": this.state.hp,
+//       "frontUrl": this.state.frontUrl,
+//       "backUrl": this.state.backUrl
+//    })
+//   });
+//  };
+
+handleSubmit = () => {
+  this.props.addNewPokemon(this.state)
+}
+
 
   render() {
     return (
@@ -19,16 +50,17 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleChange} value={this.state.name}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.handleChange} value={this.state.hp}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.handleChange} value={this.state.sprites.front}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.handleChange} value={this.state.sprites.back}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
-    )
+     )
+    }
   }
-}
+
 
 export default PokemonForm
